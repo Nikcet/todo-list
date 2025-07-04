f7974d994bd419971dd8b7563f8ea6ee0ddedf5f
Nikcet <mstislav.kr@yandex.ru>
2025-07-04 23:13:21 +0300

    for simplier imports

---
+++ todo-front/src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as TaskList } from './TaskList';
+export { default as CreateTaskForm } from './CreateTaskForm';
+export { default as AdminLogin } from './AdminLogin';
+export { default as EditableTask } from './EditableTask'; 
